049215923510c2f87af798fc8308b9658ffa8d7a
Korzhueva Daria <korzhueva.daria@gmail.com>
2022-12-17 00:17:19 +0700

    Add logging for requests

--- app/src/main/java/com/dkorzhueva/music/catalog/web/LastFmAuthorizationStorage.kt
+++ app/src/main/java/com/dkorzhueva/music/catalog/web/LastFmAuthorizationStorage.kt
@@ -2,6 +2,7 @@ package com.dkorzhueva.music.catalog.web
 
 import com.dkorzhueva.music.catalog.BuildConfig
 import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
 import java.io.IOException
@@ -11,11 +12,22 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 
+
 @Singleton
 class LastFmAuthorizationStorage @Inject constructor() : AuthorizationStorage {
-    val client = OkHttpClient.Builder()
-        .connectTimeout(15, TimeUnit.SECONDS)
-        .build()
+    //Split by classes
+
+    private fun getHttpClient(): OkHttpClient{
+        val logging = HttpLoggingInterceptor()
+        logging.apply {
+            logging.level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(logging)
+            .build()
+        return client
+    }
 
     private fun <T> createApiService(service: Class<T>, httpClient: OkHttpClient, path: String): T {
         return Retrofit.Builder()
@@ -28,46 +40,23 @@ class LastFmAuthorizationStorage @Inject constructor() : AuthorizationStorage {
 
     override suspend fun authorize(username: String, password: String) {
         val api =
-            createApiService(AuthApi::class.java, client, "https://ws.audioscrobbler.com/2.0/")
+            createApiService(AuthApi::class.java, getHttpClient(), "https://ws.audioscrobbler.com/2.0/")
         val apiSignature =
             "api_key" + BuildConfig.API_KEY + "methodauth.getMobileSessionpassword" + password + "username" + username + BuildConfig.SHARED_SECRET
 
         val codedString = getCodedString(apiSignature)
 
-        val urlParameter =
-            "method=auth.getMobileSession&api_key=" + BuildConfig.API_KEY + "&password=" + password + "&username=" + username + "&api_sig=" + codedString
-
         try {
-            //Log requests
             val result = api.authorize(BuildConfig.API_KEY, password, username, codedString)
         } catch (ex: Exception) {
             println(ex.stackTrace)
         }
-
-        val request: Request = Request.Builder()
-            .url("https://ws.audioscrobbler.com/2.0/?$urlParameter")
-            .post(RequestBody.create(null, ByteArray(0)))
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                response.body.use { responseBody ->
-                    val test = responseBody?.string()
-                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
-                }
-            }
-        })
     }
 
     private fun getCodedString(string: String): String {
-        val MD5 = "MD5"
+        val md5 = "MD5"
         try {
-            val digest = MessageDigest
-                .getInstance(MD5)
+            val digest = MessageDigest.getInstance(md5)
             digest.update(string.toByteArray())
             val messageDigest = digest.digest()
 
